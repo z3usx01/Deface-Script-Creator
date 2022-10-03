9ac6ab7e8de820e0910d72d7dbca5f99101d5b50
Z3US <112891506+z3usx01@users.noreply.github.com>
2022-10-03 20:01:55 +0530

    Update deface.py

--- deface.py
+++ deface.py
@@ -7,7 +7,6 @@ mess = """======================================================
                         Z3US.
 ======================================================"""
 
-print "mess"
 print "Created by Z3US"
 title = raw_input("title: ")
 heading = raw_input("Hacked by: ")
